ff25b9dc3eca61cf7c8293c82ba4a23f6b1b369b
Arek Ludwiniak <arkadiusz.ludwiniak1@gmail.com>
2020-06-20 23:57:10 +0200

    Adding a css styles and BEM naming

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const firstName = 'Arek';
-const age = '25';
+const firstName = "Arek";
+const age = "25";
 
 console.log(`Siema nazywam sie ${firstName} i mam ${age} lat.`);
-alert(`Siema nazywam sie ${firstName} i mam ${age} lat.`);
+//alert(`Siema nazywam sie ${firstName} i mam ${age} lat.`);
