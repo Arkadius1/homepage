1282b78426f9c72533c0eddb6dcfeb652069038b
Arek Ludwiniak <arkadiusz.ludwiniak1@gmail.com>
2020-07-09 19:24:55 +0200

    add RWD

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const firstName = "Arek";
 const age = "25";
 
+const greet = (age, firstName) => {
 console.log(`Siema nazywam sie ${firstName} i mam ${age} lat.`);
+}
 //alert(`Siema nazywam sie ${firstName} i mam ${age} lat.`);
+
+const emptyParagraph = document.querySelector('.week-summary__description--js');
+emptyParagraph.innerHTML = 'To jest dodatkowa treśc wygenerowana przez JavaScript';
+
+greet (age, firstName)
